d61ed7347f0b5a1d57b66ce5bd611ca8b2f278eb
Austin Lauritsen <austin@veryaustin.com>
2017-06-06 18:22:52 -0500

    Add menuTemplate and add Quit menu item

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  // Build Menu from template
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  
+  // Use the menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Menu Template
+const menuTemplate = [
+  // Each object represents an individual menu. In this case there will only be one menu.
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Todo'
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// If the app is runing on Mac OS, we push an extra empty object into the menuTemplate array so menu works as a native mac app should
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
